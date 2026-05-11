95ca4b65f764ead8a845bf48758ad7d0f6727db3
harishreddy-g <harishreddy.g23@gmail.com>
2026-05-11 20:52:37 +0530

    installed and tested Ejs

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
 app.get("/", (req, res) => {
     res.send("Welcome to Chatrix");
+});
+app.get("/about", (req, res) => {
+    res.send("This is my first chat application");
 });
 
 
